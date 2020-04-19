49c01adc0f5b1102b3fec82f2e9f2c77d9701005
Max Salles <herond12@gmail.com>
2020-04-19 14:43:41 -0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
+const library = {
+  name: 'myLibraryName',
+  version: '0.1.0'
+}
 
-  output: {
-    path: path.resolve('dist'),
-    filename: 'myProjectName-0.1.0.js'  
-  },
+module.exports = (_, { mode }) => {
+  const filename = (
+    `${library.name}-${library.version}${ mode === 'production' ? '.min' : '' }.js`
+  )
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {  
-          loader: 'babel-loader',  
-          options: {  
-            cacheDirectory: true  
-          }  
+  return {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+
+    output: {
+      path: path.resolve('dist'),
+      filename,
+      library: library.name,
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      globalObject: 'typeof self !== "undefined" ? self : this'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
         }
-      }  
-    ]
+      ]
+    }
   }
 }
